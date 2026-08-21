f1939628f61a151d68f619565bd069446037af4f
JongYeon <99719878+kimjy97@users.noreply.github.com>
2026-08-22 00:26:59 +0900

    fix: 뜨고 있는 음악 DND 기능 제거

--- apps/web/src/components/home/AlbumCard.jsx
+++ apps/web/src/components/home/AlbumCard.jsx
@@ -3,7 +3,6 @@ import { Play, Loader, Headphones, MoreVertical } from 'lucide-react';
 import TrackThumbnail from '../playlist/TrackThumbnail';
 import TrackActionDropdown from '../ui/TrackActionDropdown';
 import { formatArtistName } from '../../utils/trackUtils';
-import { handleTrackDragStart } from '../../utils/dragUtils';
 
 export default function AlbumCard({ track, isPlaying, onPlay, isSearching, onAddQueue }) {
   const artworkSrc = track.artwork || (track.youtube_video_id ? `https://img.youtube.com/vi/${track.youtube_video_id}/hqdefault.jpg` : null);
@@ -11,12 +10,10 @@ export default function AlbumCard({ track, isPlaying, onPlay, isSearching, onAdd
   return (
     <div 
       className={`album-card ${isPlaying ? 'album-card--playing' : ''}`}
-      draggable={true}
-      onDragStart={(e) => handleTrackDragStart(e, track)}
     >
       <div className="album-card__cover" onClick={() => onPlay(track)}>
         {artworkSrc ? (
-          <img src={artworkSrc} alt="" className="album-card__cover-img" />
+          <img src={artworkSrc} alt="" className="album-card__cover-img" draggable={false} />
         ) : (
           <TrackThumbnail
             title={track.custom_title}
